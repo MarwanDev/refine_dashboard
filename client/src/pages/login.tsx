5ca5c9afe52710d13ac1d9fc2ccdbc4056de63cb
Marwan <marwaneg@aucegypt.edu>
2023-12-25 01:36:12 +0200

    Create login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -5,82 +5,90 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";
+import { yariga } from '../assets';
 
 import { CredentialResponse } from "../interfaces/google";
 
-// Todo: Update your Google Client ID here
-const GOOGLE_CLIENT_ID =
-  "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
-
-export const Login: React.FC = () => {
-  const { mutate: login } = useLogin<CredentialResponse>();
-
-  const GoogleButton = (): JSX.Element => {
-    const divRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-      if (typeof window === "undefined" || !window.google || !divRef.current) {
-        return;
-      }
-
-      try {
-        window.google.accounts.id.initialize({
-          ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
-          callback: async (res: CredentialResponse) => {
-            if (res.credential) {
-              login(res);
-            }
-          },
-        });
-        window.google.accounts.id.renderButton(divRef.current, {
-          theme: "filled_blue",
-          size: "medium",
-          type: "standard",
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }, []);
-
-    return <div ref={divRef} />;
-  };
-
-  return (
-    <Container
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+  export const Login: React.FC = () => {
+    const { mutate: login } = useLogin<CredentialResponse>();
+    // const { mutateAsync: create } = useCreate();
+  
+    const GoogleButton = (): JSX.Element => {
+      const divRef = useRef<HTMLDivElement>(null);
+  
+      useEffect(() => {
+        if (typeof window === 'undefined' || !window.google || !divRef.current) {
+          return;
+        }
+  
+        try {
+          window.google.accounts.id.initialize({
+            ux_mode: 'popup',
+            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            callback: async (res: CredentialResponse) => {
+              if (res.credential) {
+                login(res);
+  
+                // CHANGE: add user to MongoDB
+                // const profileObj = res.credential ? parseJwt(res.credential) : null;
+                // if (profileObj) {
+                //   const { data } = await create({
+                //     resource: 'api/v1/users',
+                //     values: {
+                //       name: profileObj.name,
+                //       email: profileObj.email,
+                //       avatar: profileObj.picture,
+                //     },
+                //   });
+  
+                //   console.log('data', data);
+                // }
+              }
+            },
+          });
+          window.google.accounts.id.renderButton(divRef.current, {
+            theme: 'filled_blue',
+            size: 'medium',
+            type: 'standard',
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }, []); // you can also add your client id as dependency here
+  
+      return <div ref={divRef} />;
+    };
+  
+    return (
       <Box
-        display="flex"
-        gap="36px"
-        justifyContent="center"
-        flexDirection="column"
+        component="div"
+        sx={{ backgroundColor: '#FCFCFC' }}
       >
-        <ThemedTitleV2
-          collapsed={false}
-          wrapperStyles={{
-            fontSize: "22px",
-            justifyContent: "center",
+        <Container
+          component="main"
+          maxWidth="xs"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            height: '100vh',
           }}
-        />
-
-        <GoogleButton />
-
-        <Typography align="center" color={"text.secondary"} fontSize="12px">
-          Powered by
-          <img
-            style={{ padding: "0 5px" }}
-            alt="Google"
-            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
-          />
-          Google
-        </Typography>
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <img src={yariga} alt="Yariga Logo" />
+            <Box mt={4}>
+              <GoogleButton />
+            </Box>
+          </Box>
+        </Container>
       </Box>
-    </Container>
-  );
-};
+    );
+  };
+  
