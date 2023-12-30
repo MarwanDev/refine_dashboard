928cd98b888804af26f23bb2b9f65f8d76e7b778
Marwan <marwaneg@aucegypt.edu>
2023-12-30 06:34:11 +0200

    Create client files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,94 +1,92 @@
-import { useLogin } from "@refinedev/core";
-import { useEffect, useRef } from "react";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable react/no-unstable-nested-components */
+import { useEffect, useRef } from 'react';
+import { useLogin } from '@pankod/refine-core';
+import { Container, Box } from '@pankod/refine-mui';
 
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import { ThemedTitleV2 } from "@refinedev/mui";
+import { CredentialResponse } from 'interfaces/google';
+// import { parseJwt } from 'utils/parse-jwt';
 import { yariga } from '../assets';
 
-import { CredentialResponse } from "../interfaces/google";
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+  // const { mutateAsync: create } = useCreate();
 
-  export const Login: React.FC = () => {
-    const { mutate: login } = useLogin<CredentialResponse>();
-    // const { mutateAsync: create } = useCreate();
-  
-    const GoogleButton = (): JSX.Element => {
-      const divRef = useRef<HTMLDivElement>(null);
-  
-      useEffect(() => {
-        if (typeof window === 'undefined' || !window.google || !divRef.current) {
-          return;
-        }
-  
-        try {
-          window.google.accounts.id.initialize({
-            ux_mode: 'popup',
-            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-            callback: async (res: CredentialResponse) => {
-              if (res.credential) {
-                login(res);
-  
-                // CHANGE: add user to MongoDB
-                // const profileObj = res.credential ? parseJwt(res.credential) : null;
-                // if (profileObj) {
-                //   const { data } = await create({
-                //     resource: 'api/v1/users',
-                //     values: {
-                //       name: profileObj.name,
-                //       email: profileObj.email,
-                //       avatar: profileObj.picture,
-                //     },
-                //   });
-  
-                //   console.log('data', data);
-                // }
-              }
-            },
-          });
-          window.google.accounts.id.renderButton(divRef.current, {
-            theme: 'filled_blue',
-            size: 'medium',
-            type: 'standard',
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      }, []); // you can also add your client id as dependency here
-  
-      return <div ref={divRef} />;
-    };
-  
-    return (
-      <Box
-        component="div"
-        sx={{ backgroundColor: '#FCFCFC' }}
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === 'undefined' || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: 'popup',
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+
+              // CHANGE: add user to MongoDB
+              // const profileObj = res.credential ? parseJwt(res.credential) : null;
+              // if (profileObj) {
+              //   const { data } = await create({
+              //     resource: 'api/v1/users',
+              //     values: {
+              //       name: profileObj.name,
+              //       email: profileObj.email,
+              //       avatar: profileObj.picture,
+              //     },
+              //   });
+
+              //   console.log('data', data);
+              // }
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: 'filled_blue',
+          size: 'medium',
+          type: 'standard',
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []); // you can also add your client id as dependency here
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <Box
+      component="div"
+      sx={{ backgroundColor: '#FCFCFC' }}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
       >
-        <Container
-          component="main"
-          maxWidth="xs"
+        <Box
           sx={{
             display: 'flex',
-            flexDirection: 'column',
             justifyContent: 'center',
-            height: '100vh',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <img src={yariga} alt="Yariga Logo" />
-            <Box mt={4}>
-              <GoogleButton />
-            </Box>
+          <img src={yariga} alt="Yariga Logo" />
+          <Box mt={4}>
+            <GoogleButton />
           </Box>
-        </Container>
-      </Box>
-    );
-  };
-  
+        </Box>
+      </Container>
+    </Box>
+  );
+};
